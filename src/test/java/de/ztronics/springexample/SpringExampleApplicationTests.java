0c5f0f29f072b412f7f77d874548c10d5e0385cd
Dietmar Zabel <dietmar.zabel@gmail.com>
2024-04-28 18:23:55 +0200

    initial

---
+++ src/test/java/de/ztronics/springexample/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ztronics.springexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/de/ztronics/springexample/TestSpringExampleApplication.java
@@ -0,0 +1,23 @@
+package de.ztronics.springexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestSpringExampleApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(SpringExampleApplication::main).with(TestSpringExampleApplication.class).run(args);
+    }
+
+}
